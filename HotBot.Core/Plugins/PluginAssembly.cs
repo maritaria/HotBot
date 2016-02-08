57c9eb07f48e565f9e5c22f84ea2ee399fa971d3
mooshbeef <bramkamies@gmail.com>
2016-02-08 17:48:17 +0100

    Added new PluginLoader class which will use the AssemblyLoader to load plugins.

---
+++ HotBot.Core/Plugins/PluginAssembly.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotBot.Core.Plugins
+{
+	public interface PluginAssembly
+	{
+		string AssemblyLocation { get; }
+		bool IsLoaded { get; }
+		void Load();
+		void Unload();
+	}
+}
---
+++ HotBot.Core/Plugins/PluginLoader.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotBot.Core.Plugins
+{
+	public class PluginLoader
+	{
+		private AssemblyLoader AssemblyLoader { get; }
+
+		public string PluginDirectory { get; }
+
+		public PluginLoader(AssemblyLoader assemblyLoader)
+		{
+			if (assemblyLoader == null)
+			{
+				throw new ArgumentNullException("assemblyLoader");
+			}
+			AssemblyLoader = assemblyLoader;
+		}
+
+		public void LoadPlugins()
+		{
+			foreach(PluginAssembly assembly in FindPluginAssemblies())
+			{
+
+			}
+		}
+
+		private IEnumerable<PluginAssembly> FindPluginAssemblies()
+		{
+			return null;
+		}
+	}
+}
