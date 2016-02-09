a77208aab00d64416bb10d8c3a346370341cf2da
mooshbeef <bramkamies@gmail.com>
2016-02-09 21:21:05 +0100

    Added more emoticons to TwitchEmotes

--- HotBot.Core/Util/TwitchEmotes.cs
+++ HotBot.Core/Util/TwitchEmotes.cs
@@ -8,20 +8,22 @@ namespace HotBot.Core.Util
 {
 	public sealed class TwitchEmotes
 	{
-		public const string SmileyFace = @":)";
-		public const string SadFace = @":(";
-		public const string Surprised = @":o";
-		public const string Sleeping = @":z";
-		public const string Sunglasses = @"B)";
-		public const string MehFace = @":\";
-		public const string WinkyFace = @";)";
-		public const string WinkyLolFace = @";p";
-		public const string LolFace = @":p";
-		public const string Pirate = @"R)";
-		public const string WowFace = @"o_O";
-		public const string LaughingFace = @":D";
-		public const string AngryFace = @">(";
-		public const string Love = @"<3";
+		public static class Classic
+		{
+			public const string SmileyFace = @":)";
+			public const string SadFace = @":(";
+			public const string Surprised = @":o";
+			public const string Sleeping = @":z";
+			public const string Sunglasses = @"B)";
+			public const string MehFace = @":\";
+			public const string WinkyFace = @";)";
+			public const string WinkyLolFace = @";p";
+			public const string LolFace = @":p";
+			public const string Pirate = @"R)";
+			public const string WowFace = @"o_O";
+			public const string LaughingFace = @":D";
+			public const string AngryFace = @">(";
+		}
 
 		public static class Turbo
 		{
@@ -35,7 +37,44 @@ namespace HotBot.Core.Util
 
 		public static class Faces
 		{
+			public const string Kappa = @"Kappa";
+			public const string KappaPride = @"KappaPride";
+			public const string FaultTech = @"FaultBoy";
+		}
 
+		public static class Icons
+		{
+			public const string BleedingHeart = @"bleedPurple";
+			public const string Tornade = @"duDudu";
+			public const string PokemonShell = @"PraiseIt";
+			public const string Illuminati = @"deIlluminati";
+			public const string Love = @"<3";
+			public const string Raid = @"twitchRaid";
+			public const string Mailbox = @"BCouch";
+			public const string Dogecoin = @"ShibeZ";
+			public const string Kapow = @"KAPOW";
+			public const string Deadmau5 = @"Mau5";
+			public const string MonsterCat = @"mcaT";
+			public const string Warning = @"ItsBoshyTime";
+			public const string Skull = @"SMSkull";
+			public const string Salt = @"PJSalt";
+			public const string HolyGameController = @"HumbleLife";
+		}
+
+		public static class Monsters
+		{
+			public const string Reptile = @"OSkomodo";
+			public const string Sloth = @"OSfrog";
+			public const string Frog = @"OSsloth"; 
+			public const string Dog1 = @"OhMyDog"; 
+			public const string Dog2 = @"RalpherZ"; 
+			public const string Dog3 = @"FrankerZ";
+			public const string Dog4 = @"CorgiDerp";
+			public const string Orc = @"SMOrc";
+			public const string Raccoon = @"RaccAttack";
+			public const string Alien = @"BrainSlug";
+			public const string Robot = @"MrDestructoid";
+			public const string Creeper = @"SSSsss";
 		}
 
 	}
