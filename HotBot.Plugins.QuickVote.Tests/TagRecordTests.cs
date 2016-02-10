05092a51e4fc3ea5df1f6fb6ca182578241d022e
mooshbeef <bramkamies@gmail.com>
2016-02-10 19:46:32 +0100

    Added empty unit tests for TagRecord

---
+++ HotBot.Plugins.QuickVote.Tests/TagRecordTests.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace HotBot.Plugins.QuickVote.Tests
+{
+	[TestClass()]
+	public class TagRecordTests
+	{
+		[TestMethod()]
+		public void TagRecord_Constructor()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void MentionCount()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void IsExpired()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void IsPermanent()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void VerifyTag()
+		{
+			Assert.Fail();
+		}
+	}
+}
