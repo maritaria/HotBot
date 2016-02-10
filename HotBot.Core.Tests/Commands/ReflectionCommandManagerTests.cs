14c9a387b53d1a2d6ac6cdef920cf26c8185b2a4
mooshbeef <bramkamies@gmail.com>
2016-02-10 19:26:12 +0100

    Added unit tests for ReflectionCommandManager.

---
+++ HotBot.Core.Tests/Commands/ReflectionCommandManagerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using HotBot.Core.Commands;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotBot.Core.Commands.Tests
+{
+	[TestClass()]
+	public class ReflectionCommandManagerTests
+	{
+		[TestMethod()]
+		public void ReflectionCommandManager_Constructor()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void Register()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void Unregister()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void IsRegistered()
+		{
+			Assert.Fail();
+		}
+
+		[TestMethod()]
+		public void RunCommand()
+		{
+			Assert.Fail();
+		}
+	}
+}
