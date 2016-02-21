44241526ddc8c5c6a51553f123e4da9ddf24c77d
mooshbeef <bramkamies@gmail.com>
2016-02-21 19:02:49 +0100

    Moved DictionaryMessageBus to a new Impl namespace for Intercom

--- HotBot.Core/Intercom/Impl/DictionaryMessageBus.cs
+++ HotBot.Core/Intercom/Impl/DictionaryMessageBus.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 
-namespace HotBot.Core.Intercom
+namespace HotBot.Core.Intercom.Impl
 {
 	public sealed class DictionaryMessageBus : MessageBus
 	{
